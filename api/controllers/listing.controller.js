9dfbf0a80af8a906926e5e6315b628e87badc016
Anis <de-vil-jin@hotmail.fr>
2024-01-08 23:24:30 +0100

    create search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -66,3 +66,60 @@ export const getListing = async (req, res, next) => {
         next(error)
     }
 }
+
+//API : search listing functionality 
+export const getListings = async (req, res, next) => {
+
+    try {
+        const limit = parseInt(req.query.limit) || 9; //query.limit => ?limit=2 (query:?)
+        const startIndex = parseInt(req.query.startIndex) || 0;
+
+        let offer = req.query.offer;
+
+        if (offer === undefined || offer === 'false') {
+            offer = { $in: [false, true] };
+        }
+
+        let furnished = req.query.furnished;
+
+        if (furnished === undefined || furnished === 'false') {
+            furnished = { $in: [false, true] };
+        }
+
+        let parking = req.query.parking;
+
+        if (parking === undefined || parking === 'false') {
+            parking = { $in: [true, false] };
+        }
+
+        let type = req.query.type;
+
+        if (type === undefined || type === 'all') {
+            type = { $in: ['sale', 'rent'] };
+        }
+
+        const searchTerm = req.query.searchTerm || '';
+
+        const sort = req.query.sort || 'createdAt';
+
+        const order = req.query.order || 'desc';
+        //search
+        const listings = await Listing.find({
+            //here we limit and filter
+            name: { $regex: searchTerm, $options: 'i' }, // 'i' => ignore lowercase and uppercase
+            offer,
+            furnished,
+            parking,
+            type,
+        })  // here we order the search
+            .sort({ [sort]: order })
+            .limit(limit)
+            .skip(startIndex);
+
+
+        return res.status(200).json(listings);
+
+    } catch (error) {
+        next(error)
+    }
+}
